9e637ea6f1a832e593f2dcc835f4564296e1db52
akbarmauu <76439418+akbarmauu@users.noreply.github.com>
2021-02-13 16:16:52 +0700

    upload pertama

---
+++ pertemuan2/index.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Akbar Maulana
+203040136
+https://github.com/akbarmauu/pw2021_203040136
+pertemuan 2 - 13 Februari 2021
+Mempelajari Sintaks PHP dasar
+*/
+?>
+<?php
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+
+// penulisan sintaks PHP
+// 1.PHP didalam html
+// 2. HTML di dalam PHP
+
+// variabel dan tipe data
+// variabel
+// tidak boleh di awali dengan angka,tapi boleh mengandung angka
+
+// $nama = "Akbar Maulana";
+// echo "hallo,nama saya $nama";
+
+// operator
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// penggabungan string / concatentaion / contact
+// .
+// $nama_depan = "Akbar";
+// $nama_belakang = "Maulana";
+// echo $nama_depan ." ". $nama_belakang;
+
+// Assigment
+// =, ==, -=, *=, /=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "Akbar";
+// $nama .= " ";
+// $nama .= "Maulana";
+// echo $nama;
+
+// perbandingan
+// <, >, <=, >=, ==
+// var_dump(1 == "1");
+
+// identitas
+// ===, !==
+// var_dump(1 == "1");
+
+
+// logika
+// &&, ||, !
+$x = 30;
+var_dump($x < 20 || $x % 2 == 0);
+?>
