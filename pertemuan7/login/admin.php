70cdcb391a86afe6668ce831f40fcdbeafb272a7
akbarmauu <76439418+akbarmauu@users.noreply.github.com>
2021-03-21 21:33:52 +0700

    penyelesaian tugas

---
+++ pertemuan7/login/admin.php
@@ -0,0 +1,26 @@
+<?php
+/* 
+    Akbar Maulana
+    203040136
+    https://github.com/akbarmauu/pw2021_203040136
+    Pertemuan 7 - 20 maret 2021
+    Mempelajari mengenai GET & POST 
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+
+    <h1>Selamat Datang Admin!</h1>
+
+    <a href="login.php">Logout</a>
+    
+</body>
+</html>
