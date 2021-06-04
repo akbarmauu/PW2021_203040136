459baf03f9c351c0f93bb1117a9b2aa63fe76241
akbarmauu <76439418+akbarmauu@users.noreply.github.com>
2021-06-04 14:06:06 +0700

    penyelesaian tugas

---
+++ kuliah/pertemuan13/ajax/ajax_cari.php
@@ -0,0 +1,47 @@
+<?php
+/* 
+Akbar Maulana
+203040136
+https://github.com/akbarmauu/pw2021_203040136
+Pertemuan 13 - 30 April 2021
+Mempelajari AJAX Live Search
+*/
+?>
+
+<?php
+require '../functions.php';
+$mahasiswa = cari($_GET['keyword']);
+
+?>
+
+<table border="1" cellpadding="10" cellspacing="0">
+  <tr>
+    <th>#</th>
+    <th>Gambar</th>
+    <th>Nama</th>
+    <th>Aksi</th>
+  </tr>
+
+  <?php if (empty($mahasiswa)) : ?>
+    <tr>
+      <td colspan="4">
+        <p style="color: red; font-style: italic;">Data mahasiswa tidak ditemukan</p>
+      </td>
+    </tr>
+  <?php endif; ?>
+
+  <?php $i = 1;
+  foreach ($mahasiswa as $m) : ?>
+
+    <tr>
+      <td><?= $i++; ?></td>
+      <td><img src="img/<?= $m['gambar']; ?>" width="60"></td>
+      <td><?= $m['nama']; ?></td>
+      <td>
+        <a href="detail.php?id=<?= $m['id']; ?>"> Lihat detail </a>
+      </td>
+    </tr>
+
+  <?php endforeach; ?>
+
+</table>
---
+++ kuliah/pertemuan13/js/script.js
@@ -0,0 +1,52 @@
+
+/* 
+Akbar Maulana
+203040136
+https://github.com/akbarmauu/pw2021_203040136
+Pertemuan 13 - 30 April 2021
+Mempelajari tentang AJAX Live Search
+*/
+
+
+const tombolCari = document.querySelector('.tombol-cari');
+const keyword = document.querySelector('.keyword');
+const container = document.querySelector('.container');
+
+// hilangkan tombol cari 
+tombolCari.style.display = 'none';
+
+// event ketika kita menuliskan keyword
+keyword.addEventListener('keyup', function () {
+  // ajax
+  // xmlhttprequest
+  // const xhr = new XMLHttpRequest();
+
+  // xhr.onreadystatechange = function () {
+  //   if (xhr.readyState == 4 && xhr.status == 200) {
+  //     conatiner.innerHTML = xhr.responseText;
+  //   }
+  // };
+
+  // xhr.open('get', 'ajax/ajax_cari.php?keyword=' + keyword.value);
+  // xhr.send();
+
+  // fetch()
+  fetch('ajax/ajax_cari.php?keyword=' + keyword.value)
+    .then((response) => response.text())
+    .then((response) => (container.innerHTML = response));
+
+});
+
+
+// Preview Image untuk Tambah dan  Ubah 
+function previewImage() {
+  const gambar = document.querySelector('.gambar');
+  const imgPreview = document.querySelector('.img-preview');
+
+  const oFReader = new FileReader();
+  oFReader.readAsDataURL(gambar.files[0]);
+
+  oFReader.onload = function (oFREvent) {
+    imgPreview.src = oFREvent.target.result;
+  };
+}
